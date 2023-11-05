fda53c3040fe0afe0de8b2817d93ff590f6fdacf
nikcich <nrc170001@utdallas.edu>
2023-11-04 21:43:38 -0500

    Comments

--- Shared/Application.js
+++ Shared/Application.js
@@ -1,139 +1,141 @@
-// Importing necessary modules and dependencies
 import { MESSAGE_TYPES } from '../Shared/SharedWorkerMessageTypes.js';
 import { AppURLs } from '../Shared/AppURLs.js';
 
-// Creating a Shared Worker and obtaining its port
 const sharedWorker = new SharedWorker('../Shared/sharedWorker.js');
 const port = sharedWorker.port;
 
-// Getting references to various DOM elements
 const chatBox = document.getElementById('chatBox');
 const messageInput = document.getElementById('messageInput');
 const sendMessageButton = document.getElementById('sendMessageButton');
 const launchP = document.getElementById('launchButtonPrimary');
 const launchS = document.getElementById('launchButtonSecondary');
 const toggleTheme = document.getElementById('toggleTheme');
-const INSTANCE = window.INSTANCE; // Storing a reference to the current window instance
-const dropTarget = document; // Setting the drop target to the entire document
 
-// Adding a 'dragover' event listener to allow for drag-and-drop
-dropTarget.addEventListener('dragover', (event) => {
-    event.preventDefault(); // Prevent the default behavior to enable dropping
-});
+const dropTarget = document;
 
-// Adding a 'drop' event listener to handle the drop operation
-dropTarget.addEventListener('drop', (event) => {
-    event.preventDefault(); // Prevent the default behavior
+// Constants for CSS files
+const DARK_CSS_FILE = '../Shared/dark.css';
 
-    // Retrieve the dropped JSON data
-    const data = event.dataTransfer.getData('application/json');
-    console.log('Dropped data: ' + data);
+// Constants for the message types
+const MESSAGE_TYPE = {
+    TOGGLE_THEME: MESSAGE_TYPES.TOGGLE_THEME,
+    LOG: MESSAGE_TYPES.LOG,
+    MESSAGE: MESSAGE_TYPES.MESSAGE,
+    CLOSE: MESSAGE_TYPES.CLOSE,
+};
 
-    // Creating a draggable element and configuring it
-    const draggableElement = document.createElement('div');
-    draggableElement.className = 'draggableElement';
-    draggableElement.draggable = true;
-    draggableElement.textContent = data;
-
-    // Configuring the 'ondragstart' event for the draggable element
-    draggableElement.ondragstart = function (event) {
-        event.dataTransfer.setData('application/json', data);
-    };
-
-    // Appending the draggable element to a target div
-    const targetDiv = document.getElementById('dragDropContent'); // Replace with the actual target element
-    targetDiv.appendChild(draggableElement);
-});
-
-// Function to include a CSS file dynamically
-const includeCSS = (cssFile) => {
-    var link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.type = "text/css";
+/**
+ * Dynamically include a CSS file in the document's head.
+ * @param {string} cssFile - The URL of the CSS file to include.
+ */
+function includeCSS(cssFile) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
     link.href = cssFile;
     document.head.appendChild(link);
 }
 
-// Function to remove a CSS file dynamically
-const removeCSS = (cssFile) => {
-    var links = document.getElementsByTagName("link");
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].getAttribute("href") === cssFile) {
-            links[i].parentNode.removeChild(links[i]);
+/**
+ * Remove a dynamically included CSS file from the document's head.
+ * @param {string} cssFile - The URL of the CSS file to remove.
+ */
+function removeCSS(cssFile) {
+    const links = document.getElementsByTagName('link');
+    for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+        if (link.getAttribute('href') === cssFile) {
+            link.parentNode.removeChild(link);
             break;
         }
     }
 }
 
-// Handling messages received from the Shared Worker
-port.onmessage = function (event) {
-    const data = event.data;
+/**
+ * Create a draggable element with the given data.
+ * @param {string} data - The data to set as the element's text content.
+ * @returns {HTMLDivElement} - The created draggable element.
+ */
+function createDraggableElement(data) {
+    const draggableElement = document.createElement('div');
+    draggableElement.className = 'draggableElement';
+    draggableElement.draggable = true;
+    draggableElement.textContent = data;
 
-    if (data.type == MESSAGE_TYPES.TOGGLE_THEME) {
-        // Toggle the theme by including or removing a CSS file
-        if (data.theme == "DARK") {
-            includeCSS("../Shared/dark.css");
+    draggableElement.ondragstart = function (event) {
+        event.dataTransfer.setData('application/json', data);
+    };
+
+    return draggableElement;
+}
+
+/**
+ * Handle messages received from the Shared Worker.
+ * @param {object} data - The data received from the Shared Worker.
+ */
+function handleReceivedMessage(data) {
+    if (data.type == MESSAGE_TYPE.TOGGLE_THEME) {
+        if (data.theme == 'DARK') {
+            includeCSS(DARK_CSS_FILE);
         } else {
-            removeCSS("../Shared/dark.css");
+            removeCSS(DARK_CSS_FILE);
         }
-
-        return;
-    }
-
-    if (data.type === MESSAGE_TYPES.LOG) {
-        // Log a message to the console
+    } else if (data.type === MESSAGE_TYPE.LOG) {
         console.log(data.message);
-        return;
-    }
-
-    if (data.message) {
-        // Handle chat messages and display them in the chat box
+    } else if (data.message) {
         const message = data.message;
         chatBox.innerHTML += `<p>${message}</p>`;
-    }
-
-    if (data.connections) {
-        // Log the list of connected ports (windows/tabs) to the console
+    } else if (data.connections) {
         console.log('Connected ports:', data.connections);
     }
-};
+}
 
-// Handling the 'click' event for sending a chat message
 sendMessageButton.addEventListener('click', function () {
-    const message = INSTANCE + ": " + messageInput.value;
+    const message = `${INSTANCE}: ${messageInput.value}`;
     if (message.trim() !== '') {
-        // Send the message to the Shared Worker
-        port.postMessage({ type: MESSAGE_TYPES.MESSAGE, message: message });
+        port.postMessage({ type: MESSAGE_TYPE.MESSAGE, message });
         chatBox.innerHTML += `<p>${message}</p>`;
         messageInput.value = '';
     }
 });
 
-// Handling the 'click' event for launching the primary window
 launchP.addEventListener('click', function () {
-    const url = AppURLs["PRIMARY"];
+    const url = AppURLs.PRIMARY;
     const windowName = 'MyPopupWindow';
     const windowFeatures = 'scrollbars=yes,modal=yes,width=400,height=400';
-    const newWindow = window.open(url, windowName, windowFeatures);
+    window.open(url, windowName, windowFeatures);
 });
 
-// Handling the 'click' event for launching the secondary window
 launchS.addEventListener('click', function () {
-    const url = AppURLs["SECONDARY"];
+    const url = AppURLs.SECONDARY;
     const windowFeatures = 'scrollbars=yes,modal=yes,width=400,height=400';
     const timestamp = new Date().getTime();
-    const newWindow = window.open(url, `${timestamp}`, windowFeatures);
+    window.open(url, `${timestamp}`, windowFeatures);
 });
 
-// Handling the 'click' event for toggling the theme
 toggleTheme.addEventListener('click', function () {
-    // Send a message to the Shared Worker to toggle the theme
-    port.postMessage({
-        type: MESSAGE_TYPES.TOGGLE_THEME
-    });
+    port.postMessage({ type: MESSAGE_TYPE.TOGGLE_THEME });
 });
 
-// Handling the 'beforeunload' event to notify the Shared Worker before closing the window
-window.addEventListener('beforeunload', function () {
-    port.postMessage({ type: MESSAGE_TYPES.CLOSE, command: 'closing' });
+dropTarget.addEventListener('dragover', (event) => {
+    event.preventDefault();
 });
+
+dropTarget.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const data = event.dataTransfer.getData('application/json');
+    console.log('Dropped data: ' + data);
+    const draggableElement = createDraggableElement(data);
+    const targetDiv = document.getElementById('dragDropContent');
+    targetDiv.appendChild(draggableElement);
+});
+
+window.addEventListener('beforeunload', function () {
+    port.postMessage({ type: MESSAGE_TYPE.CLOSE, command: 'closing' });
+});
+
+// Set up a listener for messages from the Shared Worker
+port.onmessage = function (event) {
+    const data = event.data;
+    handleReceivedMessage(data);
+};
