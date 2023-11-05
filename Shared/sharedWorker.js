4b9db6015822a8b6b2c5d123dccff31eac9a5dcd
nikcich <nrc170001@utdallas.edu>
2023-11-04 21:34:25 -0500

    Comments

--- Shared/sharedWorker.js
+++ Shared/sharedWorker.js
@@ -1,4 +1,8 @@
-// sharedWorker.js
+
+/** 
+ *   Types of messages supported
+ */
+
 const MESSAGE_TYPES = {
     FOCUS: "focus",
     MESSAGE: "message",
@@ -7,9 +11,18 @@ const MESSAGE_TYPES = {
     TOGGLE_THEME: "theme",
 };
 
+// Global theme variable with default set to LIGHT
 var THEME = "LIGHT";
 
+/**
+ * Port mapping maintains a mapping of IDs to their port objects
+ */
+
 let portMapping = {};
+
+/**
+ * This function is used to create a "unique" ID for each port connection of the SharedWorker instance
+ */
 
 const generateUniqueID = () => {
     const timestamp = new Date().getTime();
@@ -23,19 +36,35 @@ const generateUniqueID = () => {
     }
 }
 
-self.onconnect = function (e) {
-    const port = e.ports[0];
-    const connectionId = generateUniqueID();
-    portMapping[connectionId] = port;
+/**
+ * When a connection is established between a SharedWorker and a client instance
+ * The follow function is executed setting up the messaging capabilities
+ */
 
+self.onconnect = function (e) {
+    const port = e.ports[0];                    // Get the port
+    const connectionId = generateUniqueID();    // Generate ID
+    portMapping[connectionId] = port;           // Store connection port in portMapping
+
+    // Send the current selected theme immediately upon client connection
     port.postMessage({ type: MESSAGE_TYPES.TOGGLE_THEME, theme: THEME });
 
+    /**
+     * 
+     * @param {*} message 
+     * Send the log message to each connected client for logging purposes
+     */
     const logToMainConsole = (message) => {
         for (let port of Object.values(portMapping)) {
             port.postMessage({ type: MESSAGE_TYPES.LOG, message: message });
         }
     };
 
+    /**
+     * 
+     * @param {*} message 
+     * Handle incoming messages from clients, sending it to all OTHER clients.
+     */
     const handleMessage = (message) => {
         // Send the message to all connected applications, excluding the sender
         for (const connectedPort of Object.values(portMapping)) {
@@ -54,11 +83,17 @@ self.onconnect = function (e) {
         }
     }
 
+    /**
+     * Close the connection and log that a connection is closing on all connection clients
+     */
     const handleClose = () => {
         delete portMapping[connectionId];
         logToMainConsole('CLOSING ' + connectionId);
     }
 
+    /**
+     * Toggle the active theme and send messages to connected clients with the new theme
+     */
     const handleToggleTheme = () => {
         THEME = THEME === 'LIGHT' ? 'DARK' : 'LIGHT';
 
@@ -67,19 +102,30 @@ self.onconnect = function (e) {
         }
     }
 
+    /**
+     * 
+     * @param {*} event 
+     * 
+     * Route the incoming message based on the type property expected to be in the message
+     */
     port.onmessage = function (event) {
         const message = event.data;
 
-        if (message.type == MESSAGE_TYPES.MESSAGE) {
+        if (message?.type == MESSAGE_TYPES.MESSAGE) {
             handleMessage(message);
-        } else if (message.type == MESSAGE_TYPES.CLOSE) {
+        } else if (message?.type == MESSAGE_TYPES.CLOSE) {
             handleClose();
-        } else if (message.type == MESSAGE_TYPES.TOGGLE_THEME) {
+        } else if (message?.type == MESSAGE_TYPES.TOGGLE_THEME) {
             handleToggleTheme();
         }
 
     };
 
+    /**
+     * @param {*} event 
+     * 
+     * If there is an error with messaging, close the connection.
+     */
     port.onmessageerror = function (event) {
         handleClose();
     };
